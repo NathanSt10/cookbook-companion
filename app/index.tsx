a51461c7f302c507e9ba1a249ea28dba4f05ae9e
Victoria <leonardv@uwm.edu>
2025-10-01 21:04:08 -0500

    Added signup page and added links between login and signup pages

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from "expo-router";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, View } from "react-native";
-import { auth } from "/home/henry-xiong/VSCodeProjects/cookbook-companion/firebase.js";
+import { auth } from "../firebase.js";
 
 export default function Index() {
   const [user, setUser] = useState<User | null | undefined>(undefined);
@@ -21,8 +21,8 @@ export default function Index() {
   }
 
   if (user === null) {
-    return <Redirect href="/(auth)/login" />;
+    return <Redirect href="./(auth)/login" />;
   }
 
-  return <Redirect href="/(tabs)/home" />;
+  return <Redirect href="./(tabs)/home" />;
 }
--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCVi19snYV7mD3aY7xjmCkwWqzl4VT9Ehc",
@@ -13,6 +14,8 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { app, auth };
+export { auth, db };
+export default app;
